0a56b1b1198434db4b2950c4ab408778b282acda
lux-lu <67232749+lux-lu@users.noreply.github.com>
2020-06-25 14:51:37 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyCVkRcSiF0KmLCwmoo4edtw0vnSykCvFcc');
-define('GJ_CODE', 'US');
-define('SITE_NAME', 'y2b');
-define('TITLENAME', 'y2b');
-define('EN2DEKEY', '32123wwerwer323tgfdgds');
-define('EMAIL', '1334347212@qq.com');
+define('APIKEY', 'AIzaSyC4a_vjepaxFUxaWpa8kTyeBixK2BHcLyM');
+define('GJ_CODE', 'jp');
+define('SITE_NAME', 'ybv');
+define('TITLENAME', 'ybv');
+define('EN2DEKEY', 'hello12345678');
+define('EMAIL', '1604012921@qq.com');
 define('NAME', 'admin');
-define('PASSWORD', 'admin');
+define('PASSWORD', '12345678');
 define('LOGINHTML', '<!DOCTYPE html>
 <html lang="en">
 <head>
